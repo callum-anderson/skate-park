f6edb37627d6c1f3ee0b72c482b6fe8ca1c878d9
Callum Anderson <callum_anderson@hotmail.com>
2020-11-22 22:46:06 +0000

    Added API key to connect contact form with Azure Logic App

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
 @Component({
   selector: 'contact-page',
@@ -8,14 +9,26 @@ import { NgForm } from '@angular/forms';
 })
 export class ContactPageComponent implements OnInit {
   contactForm: NgForm;
+  formValues;
+  formSubmitStatus = 'default';
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(form){
-    console.log(form);
+    this.formValues = form.value;
+    this.formSubmitStatus = 'pending';
+    const formValue = JSON.stringify(this.formValues);
+    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
+    this.http.post('https://prod-20.ukwest.logic.azure.com:443/workflows/9f85b9a3c4e24307b085f72a088a5128/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=94Qm8UPxDzWoRbJx6ZTAa6xAY4ewKkKMZp0gFDPk2AE', 
+          formValue, {headers: headers})
+      .subscribe(res => {
+          this.formSubmitStatus = 'submitted';
+        },
+        err => {
+          this.formSubmitStatus = (err.status == 200) ? 'submitted' : 'error';
+        });
   }
 
 }
