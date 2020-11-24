0bb3ebf8255d0454ac53a9f931affbede1ac83eb
Callum Anderson <callum_anderson@hotmail.com>
2020-11-24 19:46:22 +0000

    Layout & font size changes across whole site

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
